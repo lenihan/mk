15bc8c0b7c536d41920c45b5ffefeb81ceed32e4
David Lenihan <22015149+lenihan@users.noreply.github.com>
2020-08-25 22:07:32 -0700

    Grab visual studio variables

--- mk.ps1
+++ mk.ps1
@@ -53,40 +53,64 @@ Param()
 
 
 # TODO: command line option to force recreate $home/mk.psd1
-
+$mkvar = @{}
 $rootDataFile = "$HOME/mk.psd1"
 if (!(Test-Path $rootDataFile)) {
     if ($IsWindows) {
-        Write-Host "Looking for Visual Studio installs..." 
-        $vcvarsall = (Get-ChildItem -Path ${env:ProgramFiles(x86)} -Filter vcvarsall.bat -Recurse -ErrorAction SilentlyContinue).FullName
-        if ($vcvarsall.count -eq 0) {
-            Write-Host "ERROR: Could not find a Visual Studio installation in ${env:ProgramFiles(x86)}" -ForegroundColor Red
+        Write-Host "Looking for Visual Studio installs..."
+        $vcbatName = "VsDevCmd.bat" 
+        $vcbat = (Get-ChildItem -Path ${env:ProgramFiles(x86)} -Filter $vcbatName -Recurse -ErrorAction SilentlyContinue).FullName
+        if ($vcbat.count -eq 0) {
+            Write-Host "ERROR: Could not find Visual Studio file $vcbatName in ${env:ProgramFiles(x86)}" -ForegroundColor Red
             return
         }
-        elseif ($vcvarsall.count -ne 1) {
-            for ($i = 0; $i -lt $vcvarsall.count; $i++) {
-                Write-Host "[$i] $($vcvarsall[$i])"
+        elseif ($vcbat.count -ne 1) {
+            for ($i = 0; $i -lt $vcbat.count; $i++) {
+                Write-Host "[$i] $($vcbat[$i])"
             }
             $num = Read-Host -Prompt "Multiple installations found. Enter number of installation to use"
-            $vcvarsall = $vcvarsall[$num]
+            $vcbat = $vcbat[$num]
         }
-        Write-Host "Found: $vcvarsall" -ForegroundColor Green
+        Write-Host "Found: $vcbat" -ForegroundColor Green
+
+        # TODO - # Store mkvar in $rootDataFile
+        # TODO find cl path
+        # TODO find link path
+        $command = '"{0}" & set' -f $bat
+        & cmd /c $command | ForEach-Object {
+            if ($_ -match "=") {
+                $name, $value = $_.split("=")
+                $mkvar[$name] = $value
+            }
+        }
+        
+        $mkvar
+        # TODO
+        # WindowsSDKLibVersion
+        # INCLUDE
+        # LIBPATH
+        # VCToolsVersion
+        # WindowsSdkVerBinPath
+        # NETFXSDKDir
+        # WT_SESSION
+        # FrameworkVersion32
+        # __DOTNET_ADD_32BIT
+        # WindowsSdkDir
+        # VSCMD_ARG_HOST_ARCH
+        # ExtensionSdkDir
+        # WindowsLibPath
+        # FrameworkVersion
+        # VCINSTALLDIR
+        # VSINSTALLDIR
+        # WindowsSdkBinPath
+        # LIB
+        # FSHARPINSTALLDIR
+        # Framework40Version
+        # VSCMD_VER
+        # LANG
     }
 }
 
-
- 
-        # TODO: path to cl
-#Set environment variables for Visual Studio Command Prompt
-# pushd 'c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC'
-# cmd /c "vcvarsall.bat&set" |
-# foreach {
-#   if ($_ -match "=") {
-#     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-#   }
-# }
-# popd
-# write-host "`nVisual Studio 2010 Command Prompt variables set." -ForegroundColor Yellow
 
 
 
